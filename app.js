9f2bfdcd6b50dbbdc86b6803a71b7d2197f840f7
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-02-09 11:23:00 +0000

    codigo que funciona con extensiones de dominios

---
+++ app.js
@@ -0,0 +1,23 @@
+let pronom = ['El', 'Nuestro', 'Vuestro' ];
+
+let adj = ['único', 'incomparable', 'maravilloso'];
+
+let nomb = ['boss', 'nerd' ,'puto amo'];
+
+let dom = ['.es','.hack','.apok','.com' ]
+
+    for (let i = 0; i < pronom.length; i++) {
+         
+        for (let j = 0; j < adj.length; j++) {
+
+        for (let k = 0; k < nomb.length; k++) {
+    
+        for (let l = 0; l < nomb.length; l++) {
+        
+    
+     console.log(pronom[i]+adj[j]+nomb [k]+ dom[l]);
+    
+     }
+    } 
+  } 
+}
